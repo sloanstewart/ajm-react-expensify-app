12de9effa6948dbe93a97e8c180035586bb4774a
sloanstewart <sloanstewartdev@gmail.com>
2018-01-08 11:20:10 -0500

    Redux counter

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,11 +1,44 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }) => {
-  return state;
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    case 'RESET':
+      return {
+        count: state.count = 0
+      };
+    default:
+      return state;
+  }
+
 });
 
 console.log(store.getState());
 
 // Actions
 // I'd like to increment the count
+store.dispatch({
+  type: 'INCREMENT',
+});
+
+// Reset count 
+
+store.dispatch({
+  type: 'RESET',
+});
+
 // I'd like to reset the count to zero
+
+store.dispatch({
+  type: 'DECREMENT',
+});
+
+
+console.log(store.getState());
