28ab91730252f32a86c4af7ea190f59d5d1f7d90
Sloan Stewart <sloanstewart@d25sp18agq17im.amer.homedepot.com>
2018-01-19 15:23:52 -0500

    Added tests for expense dashboard, list, list item, header, and not found page

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
 
-const ExpenseDashboardPage = () => (
+export const ExpenseDashboardPage = () => (
   <div>
-    <ExpenseListFilters/>
+    <ExpenseListFilters />
     <ExpenseList />
   </div>
 );
