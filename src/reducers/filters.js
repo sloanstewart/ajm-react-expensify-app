24e012399e8d54b200b89a140a4135e2b6f62251
Sloan Stewart <sloanstewart@d25sp18agq17im.amer.homedepot.com>
2018-01-19 11:24:00 -0500

    Added tests for filters reducer

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -14,29 +14,29 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
       return {
         ...state,
         text: action.text,
-      }
+      };
     case 'SORT_BY_DATE':
       return {
         ...state,
         sortBy: 'date',
-      }
+      };
     case 'SORT_BY_AMOUNT':
       return {
         ...state,
         sortBy: 'amount',
-      }
+      };
     case 'SET_START_DATE':
-     return {
-       ...state,
-       startDate: action.date,
-     }
+      return {
+        ...state,
+        startDate: action.date,
+      };
     case 'SET_END_DATE':
-     return {
-       ...state,
-       endDate: action.date,
-     }
+      return {
+        ...state,
+        endDate: action.date,
+      };
     default:
-     return state;
+      return state;
   }
 };
 
